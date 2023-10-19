0ac87be3d6be3cf3749adf20475cfb6b801a4d2d
Kostadin Rizov <rizov.kostadin@gmx.at>
2023-10-19 20:47:27 +0200

    first commit

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -11,17 +11,20 @@ public class CalculatorTest {
 
     @Test
     void testSubtract() {
-        Assertions.fail("Test not implemented");
+        double result = Calculator.subtract(8.2, 3.7);
+        Assertions.assertEquals(4.5, result, 0.0001);
     }
 
     @Test
     void testMultiply() {
-        Assertions.fail("Test not implemented");
+        double result = Calculator.multiply(2.5, 3.7);
+        Assertions.assertEquals(9.25, result, 0.0001);
     }
 
     @Test
     void testDivide() {
-        Assertions.fail("Test not implemented");
+        double result = Calculator.divide(8.0, 2.5);
+        Assertions.assertEquals(3.2, result, 0.0001);
     }
 
     /**
@@ -29,13 +32,21 @@ public class CalculatorTest {
      */
     @Test
     void testDivideByZeroException() {
-        Assertions.fail("Test not implemented");    }
+        Assertions.assertThrows(ArithmeticException.class, () -> {
+            Calculator.divide(8.0, 0);
+        });
+    }
 
     /**
      * checks if the correct error message is thrown
      */
     @Test
     void testDivideByZeroExceptionMessage() {
-        Assertions.fail("Test not implemented");
+        ArithmeticException exception = Assertions.assertThrows(ArithmeticException.class, () -> {
+            Calculator.divide(8.0, 0);
+        });
+        String expectedMessage = "Cannot divide by zero";
+        String actualMessage = exception.getMessage();
+        Assertions.assertTrue(actualMessage.contains(expectedMessage));
     }
 }
